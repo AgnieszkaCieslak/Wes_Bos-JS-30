9b0369844064ec3b682e0e0d3ec47f78cf144c75
Agnieszka Lewandowska Cieślak <agnieszka.lewandowska.cieslak@gmail.com>
2023-02-28 12:58:35 +0100

    pierwszy dzień

---
+++ JS/DAY-1.js
@@ -0,0 +1,6 @@
+const log = (param, text = "RESULT: ") => {
+  console.log(text, param);
+};
+log(true, `test`);
+log(`---------------------------------------------------`);
+log(`JAVA_SCRIPT_30---------------DAY 1`);
---
+++ JS/DAY-2.js
@@ -0,0 +1,7 @@
+const log = (param, text = "RESULT: ") => {
+    console.log(text, param);
+  };
+  log(true, `test`);
+  log(`---------------------------------------------------`);
+  log(`JAVA_SCRIPT_30---------------DAY 2`);
+  
